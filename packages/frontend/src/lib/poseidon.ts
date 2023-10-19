866182ee9cac088a11b264e411d45e7b46d69440
maggo <me@mar.co.de>
2023-10-19 18:27:05 +0200

    fix(frontend): build issues

--- packages/frontend/src/lib/poseidon.ts
+++ packages/frontend/src/lib/poseidon.ts
@@ -4,10 +4,13 @@ import { Hex } from 'viem';
 export const P =
   21888242871839275222246405745257275088548364400416034343698204186575808495617n;
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type PoseidonHashFn = (inputs: any[]) => Uint8Array;
 export type Poseidon = PoseidonHashFn & {
   F: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     e: (hex: string) => any;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     toString: (input: any, radix: number) => string;
   };
 };
