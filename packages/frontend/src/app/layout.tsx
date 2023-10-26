da13ff5d7634cad6f1e4e44b49a124d6087ad637
maggo <me@mar.co.de>
2023-10-26 19:26:56 +0200

    fix(frontend): page title

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from '~/components/ui/toaster';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'zkVRF',
+  title: 'zkVRF Explorer',
   description: 'decentralized verifiable randomness',
   icons:
     'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎲</text></svg>',
